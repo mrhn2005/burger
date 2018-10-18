22a3402bc0e2d51fd6a607ad165357e67bd457b1
Masoud <mrhn2005@gmail.com>
2018-10-19 01:10:52 +0330

    not correct

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import Aux1 from '../../hoc/Aux1';
+import classes from './Layout.css';
 const layout=(props)=>(
     <Aux1>
         <div>Toolbar</div>
-        <main>
+        <main className={classes.Content}>
             {props.children}
         </main>
     </Aux1>
---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Aux1 from '../../hoc/Aux1';
+
+class BurgerBuilder extends Component{
+    render(){
+        return (
+            <Aux1>
+                <div>Burger</div>
+                <div>BurgerBuilder</div>
+            </Aux1>
+        );
+    };
+}
+
+export default BurgerBuilder;
